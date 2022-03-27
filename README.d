d59a7b4fcfceb294ac150501fa4d4b607e8df866
Luke Foreman <github@4man.dev>
2022-03-27 13:52:10 +0100

    Readme

---
+++ README.d
@@ -0,0 +1,14 @@
+# About
+
+Solidify is a dotnet 5 console app to turn .epub files downloaded from `Course > Resources > Downloads > Ebook (EPUB)` into markdown files that can be imported into Obsidian.
+
+----
+
+## Usage
+1. `git clone https://github.com/LukeThoma5/Solidify.git`
+2. `cd Solidify`
+3. `dotnet run -- "path-to-epub"`
+
+A folder containing the markdown files will be placed in the same directory as the epub.
+
+Works with individual epub files or a zipped archive of them
